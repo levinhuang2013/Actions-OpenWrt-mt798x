f5fc22f8e40600e65fd327364c27b65e043139da
levinhuang2013 <152769562+levinhuang2013@users.noreply.github.com>
2024-06-13 18:37:30 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -15,7 +15,7 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app
 git clone https://github.com/messense/aliyundrive-webdav package/aliyundrive-webdav
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
